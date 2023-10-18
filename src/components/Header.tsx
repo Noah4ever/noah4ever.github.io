b084c89121e97dbf336161b6c4f42800451c3ac2
Noah <noah.thiering@web.de>
2023-10-18 09:04:35 +0200

    feature: add color theme switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,22 @@
 import "@styles//header.scss";
 import { useState } from "react";
-import { HiOutlineMenuAlt3 as Menu, HiX as Close } from "react-icons/hi";
+import {
+  HiOutlineMenuAlt3 as Menu,
+  HiX as Close,
+  HiSun as Sun,
+  HiOutlineSun as Moon,
+} from "react-icons/hi";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [lightMode, setLightMode] = useState(true);
   function handleMenuClick() {
     document.querySelector(".header-links")?.classList.toggle("open");
     setMenuOpen(!menuOpen);
   }
-  function handleLinkClick() {
-    document.querySelector(".header-links")?.classList.remove("open");
-    setMenuOpen(false);
+  function handleLightMode() {
+    document.querySelector("body")?.classList.toggle("dark");
+    setLightMode(!lightMode);
   }
 
   return (
@@ -29,10 +35,15 @@ export default function Header() {
           <Menu className="header-link-menu" onClick={handleMenuClick} />
         )}
         <ul className="header-links">
-          <li onClick={handleLinkClick}>timeline</li>
-          <li onClick={handleLinkClick}>about</li>
-          <li onClick={handleLinkClick}>skills</li>
-          <li onClick={handleLinkClick}>contact</li>
+          <li onClick={handleMenuClick}>timeline</li>
+          <li onClick={handleMenuClick}>about</li>
+          <li onClick={handleMenuClick}>skills</li>
+          <li onClick={handleMenuClick}>contact</li>
+          <li onClick={handleLightMode}>
+            <div className="header-links-theme-change">
+              {lightMode ? <Moon /> : <Sun />}
+            </div>
+          </li>
         </ul>
       </div>
     </header>
