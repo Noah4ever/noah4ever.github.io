d2857cd0ffea829ae16805c69ea4e794b61db8fb
Noah <noah.thiering@web.de>
2023-11-23 16:00:50 +0100

    refactor: emoji spacing

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -21,11 +21,11 @@ export default function Content() {
   return (
     <div className="content-container">
       <div className="content content-timeline-container">
-        <h2 id="timeline">🚧timeline👷</h2>
-        <p>🦺Under construction!</p>
+        <h2 id="timeline">🚧 timeline 👷</h2>
+        <p>🦺 Under construction!</p>
       </div>
       <div className="content content-biography-container">
-        <h2 id="about">🔎about</h2>
+        <h2 id="about">🔎 about</h2>
         <p>
           I have been developing my skills since 2021, after completing my
           initial training. I specialize in web development using TypeScript and
@@ -34,12 +34,11 @@ export default function Content() {
         </p>
       </div>
       <div className="content content-skills-container">
-        <h2 id="skills">🚧skills👷</h2>
-
-        <p>🦺Under construction!</p>
+        <h2 id="skills">🚧 skills 👷</h2>
+        <p>🦺 Under construction!</p>
       </div>
       <div className="content content-contact-container">
-        <h2 id="contact">📧contact</h2>
+        <h2 id="contact">📧 contact</h2>
         <Contact />
       </div>
     </div>
